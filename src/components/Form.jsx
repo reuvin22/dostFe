395cca4b9143aa3d3c9b1264f426ddeb2b068628
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-01-14 19:06:25 +0800

    Update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -34,22 +34,22 @@ function Form() {
         <div className="flex items-center justify-center w-screen h-screen border-yellow border-8 border-orange-500">
           <form className='grid border-slate-500 
           sm:w-11/12 md:w-2/4 lg:w-2/4 xl:w-96 
-          sm:h-11/12 md:w-2/4 xl:h-4/6 border-2 rounded-lg' onSubmit={handleSubmit}>
+          sm:h-11/12 xl:h-4/6 border-2 rounded-lg gap-2' onSubmit={handleSubmit}>
             <legend className='text sm:text-2xl md:text-3xl xl:text-3xl font-bold text-blue-500 place-self-center my-6'>DOST ATTENDANCE</legend>
-            <label className='text-2xl font-semibold mx-10 sm:justify-self-center' htmlFor="fname">First Name</label>
-            <input type="text" name='firstName' className='border-2 rounded-lg h-9 w-3/4 px-5 mx-10 sm:justify-self-center' value={formData.firstName} onChange={handleInput}/>
-            <label className='text-2xl font-semibold sm:justify-self-center mx-10' htmlFor="lname">Last Name</label>
-            <input type="text" name='lastName' className='border-2 rounded-lg h-9 w-3/4 px-5 mx-10 sm:justify-self-center' value={formData.lastName} onChange={handleInput}/>
-            <label className='text-2xl font-semibold mx-10 sm:justify-self-center' htmlFor="school">School</label>
-            <input type="text" name='school' className='border-2 rounded-lg h-9 w-3/4 px-5 mx-10 sm:justify-self-center' value={formData.school} onChange={handleInput}/>
-            <label className='text-2xl font-semibold mx-10 sm:justify-self-center' htmlFor="school">Age</label>
-            <input type="text" name='age' className='border-2 rounded-lg h-9 w-3/4 px-5 mx-10 sm:justify-self-center' value={formData.age} onChange={handleInput}/>
-            <label className='text-2xl font-semibold mx-10 sm:justify-self-center' htmlFor="contactNumber">Contact Number</label>
-            <input type="text" name='contactNumber' className='border-2 rounded-lg h-9 w-3/4 px-5 mx-10 sm:justify-self-center' value={formData.contactNumber} onChange={handleInput}/>
-            <label className='text-2xl font-semibold mx-10 sm:justify-self-center' htmlFor="gender">Gender</label>
-            <input type="text" name='gender' className='border-2 rounded-lg h-9 w-3/4 px-5 mx-10 sm:justify-self-center' value={formData.gender} onChange={handleInput}/>
-            <label className='text-2xl font-semibold mx-10 sm:justify-self-center' htmlFor="grade">Grade</label>
-            <input type="text" name='grade' className='border-2 rounded-lg h-9 w-3/4 px-5 mx-10 sm:justify-self-center' value={formData.grade} onChange={handleInput}/>
+            <label className='label' htmlFor="fname">First Name</label>
+            <input type="text" name='firstName' className='input' value={formData.firstName} onChange={handleInput}/>
+            <label className='label' htmlFor="lname">Last Name</label>
+            <input type="text" name='lastName' className='input' value={formData.lastName} onChange={handleInput}/>
+            <label className='label' htmlFor="school">School</label>
+            <input type="text" name='school' className='input' value={formData.school} onChange={handleInput}/>
+            <label className='label' htmlFor="school">Age</label>
+            <input type="text" name='age' className='input' value={formData.age} onChange={handleInput}/>
+            <label className='label' htmlFor="contactNumber">Contact Number</label>
+            <input type="text" name='contactNumber' className='input' value={formData.contactNumber} onChange={handleInput}/>
+            <label className='label' htmlFor="gender">Gender</label>
+            <input type="text" name='gender' className='input' value={formData.gender} onChange={handleInput}/>
+            <label className='label' htmlFor="grade">Grade</label>
+            <input type="text" name='grade' className='input' value={formData.grade} onChange={handleInput}/>
             <button className='h-10 w-1/2 justify-self-center my-5 bg-blue-300 rounded-2xl text-white font-semibold' name='submit'>Submit</button>
           </form>
         </div>
